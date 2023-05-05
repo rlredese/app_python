f22de85fb6506c1684ff2654388218f79591e0ef
rlipert2im <masterpull@2im.com.br>
2023-05-05 01:14:46 -0300

    Add variavel para nove e versao do APP

--- build.sh
+++ build.sh
@@ -3,11 +3,13 @@
 #Imagem1 Sistema base debian 10 com python3.6
 #Imagem2 código python atualizado
 # "full" para build completo e code para atualizar somente o código
-
+IMAGE_NAME="app-python"
+VERSION=1.0
 if [ $1 = "full" ] ; then
+    echo "Iniciando o build da imagem base..."
     docker build -f Dockerfile.base -t debian-base:1.0
-    docker build . -t app_python:1.0
+    docker build . -t $IMAGE_NAME:$VERSION
 fi
 if [ $1 = "code" ];then
-    docker build . -t app_python:1.0
+    docker build . -t $IMAGE_NAME:$VERSION
 fi
